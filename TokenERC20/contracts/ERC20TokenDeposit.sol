fe6f586fa8f74bea26ea1bd48af3bf51d960f127
Igor <eternal.dragon1288@gmail.com>
2021-08-09 23:34:15 +0300

    Homework4

--- TokenERC20/contracts/ERC20TokenDeposit.sol
+++ TokenERC20/contracts/ERC20TokenDeposit.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Deposit {
+contract TokenDeposit {
     IERC20 public coin;
     address private _owner;
 
